16ceeca2c06f38fd3a8eb72f73bd29bc732e5e22
Vincent Bryan F. Calija <vincentbryan.c@agentsofvalue.com>
2020-07-24 18:12:01 +0800

    loan billing and financials initial update

--- pages/company/loan/billings/index.vue
+++ pages/company/loan/billings/index.vue
@@ -1,10 +1,62 @@
 <template>
-    
+    <a-layout :style="{ minHeight: '100vh' }">
+        <AppHeader />
+        <a-layout :style="{ marginTop: '64px' }">
+            <AppSider />
+            <a-layout :style="{ padding: '0 24px 24px' }">
+                <a-breadcrumb :style="{ margin: '16px 0' }">
+                    <a-breadcrumb-item>Company Name</a-breadcrumb-item>
+                    <a-breadcrumb-item>Loan</a-breadcrumb-item>
+                    <a-breadcrumb-item>Billings</a-breadcrumb-item>
+                </a-breadcrumb>
+                <a-layout-content class="layout-content">
+                    <div>
+                        <h1 class="title">Billings</h1>
+                    </div>
+                    <div>
+                        <a-tabs default-active-key="1">
+                            <a-tab-pane key="1" tab="summary">
+                                Summary
+                            </a-tab-pane>
+                            <a-tab-pane key="2" tab="clientinvoice" force-render>
+                                Client invoice
+                            </a-tab-pane>
+                        </a-tabs>
+                    </div>
+                </a-layout-content>
+            </a-layout>
+        </a-layout>
+        <AppFooter />
+    </a-layout>
 </template>
 
 <script>
-export default {
+import AppHeader from '../../../../components/company/AppHeader';
+import AppSider from '../../../../components/company/AppSider';
+import AppFooter from '../../../../components/AppFooter';
+// import LoanBillingSummary from '../../../../components/company/loan/billings/LoanBillingSummary';
+// import LoanBillingClientInvoice from '../../../../components/company/loan/billings/LoanBillingClientInvoice';
 
+export default {
+    head() {
+        return {
+            title: 'Company Loan Billings',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: ''
+                }
+            ]
+        }
+    },
+    components: {
+        AppHeader,
+        AppSider,
+        AppFooter,
+        // LoanBillingSummary,
+        // LoanBillingClientInvoice,
+    },
 }
 </script>
 
--- pages/company/loan/financials/index.vue
+++ pages/company/loan/financials/index.vue
@@ -1,10 +1,48 @@
 <template>
-    
+    <a-layout :style="{ minHeight: '100vh' }">
+        <AppHeader />
+        <a-layout :style="{ marginTop: '64px' }">
+            <AppSider />
+            <a-layout :style="{ padding: '0 24px 24px' }">
+                <a-breadcrumb :style="{ margin: '16px 0' }">
+                    <a-breadcrumb-item>Company Name</a-breadcrumb-item>
+                    <a-breadcrumb-item>Loan</a-breadcrumb-item>
+                    <a-breadcrumb-item>Financials</a-breadcrumb-item>
+                </a-breadcrumb>
+                <a-layout-content class="layout-content">
+                    <div>
+                        <h1 class="title">Financials</h1>
+                    </div>
+                </a-layout-content>
+            </a-layout>
+        </a-layout>
+        <AppFooter />
+    </a-layout>
 </template>
 
 <script>
-export default {
+import AppHeader from '../../../../components/company/AppHeader';
+import AppSider from '../../../../components/company/AppSider';
+import AppFooter from '../../../../components/AppFooter';
 
+export default {
+    head() {
+        return {
+            title: 'Company Loan Financials',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: ''
+                }
+            ]
+        }
+    },
+    components: {
+        AppHeader,
+        AppSider,
+        AppFooter,
+    },
 }
 </script>
 
